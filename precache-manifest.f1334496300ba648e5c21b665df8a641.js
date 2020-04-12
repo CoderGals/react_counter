dcc98d5ac4820c4a723426262cfac35adbad4f90
Albiona Hoti <albionahh@outlook.com>
2020-04-12 23:44:28 +0200

    Updates

---
+++ precache-manifest.f1334496300ba648e5c21b665df8a641.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79268fad89717fac6681fc74af9f2f7c",
+    "url": "/react_counter/index.html"
+  },
+  {
+    "revision": "6397c766c01bee7dee6a",
+    "url": "/react_counter/static/css/main.3efab5a3.chunk.css"
+  },
+  {
+    "revision": "e103eaf7c06a1dd02b01",
+    "url": "/react_counter/static/js/2.68cce5df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_counter/static/js/2.68cce5df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6397c766c01bee7dee6a",
+    "url": "/react_counter/static/js/main.20c8da43.chunk.js"
+  },
+  {
+    "revision": "d86967b7549e4761cd3d",
+    "url": "/react_counter/static/js/runtime-main.41b96211.js"
+  }
+]);
